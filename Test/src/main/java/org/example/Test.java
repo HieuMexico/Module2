2d6b448c165801172c04c5966770ad910ed043aa
HieuMexico <lidepzai75@gmail.com>
2023-03-14 08:36:41 +0700

    hihi

---
+++ Test/src/main/java/org/example/Test.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Test {
+    public static void main(String[] args) {
+        String string = "Học lập trình là một hạnh phúc";
+        System.out.printf("%S\n", string);
+    }
+}
+
